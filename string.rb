f8f7482530a85f46850fe3a0fd19692ea6cd0ad9
Justin Bishop <jubishop@gmail.com>
2019-06-12 13:21:21 -0700

    return answer in sliding window

--- string.rb
+++ string.rb
@@ -3,7 +3,7 @@ class String
     (0...length).each { |reduction|
       reduced_length = length - reduction
       0.upto(reduction) { |left|
-        return if yield self[left, reduced_length]
+        return self[left, reduced_length] if yield self[left, reduced_length]
       }
     }
   end
