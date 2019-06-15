c45d4734fb480e0b6f8b1f2b4220e870ec516bc9
Justin Bishop <jubishop@gmail.com>
2019-06-15 12:04:57 -0700

    cleaner trie

--- trie.rb
+++ trie.rb
@@ -1,10 +1,44 @@
-class String
-  def each
-    each_char { |char| yield char }
-  end
-end
-
 class Trie
+  def initialize(list_of_values = [])
+    @head = TrieNode.new
+    list_of_values.each { |values| add_path(values) }
+  end
+
+  def has_prefix(values)
+    cur = @head
+    values.each { |value|
+      cur = cur[value]
+      return false if cur.nil?
+    }
+    return true
+  end
+
+  def add_path(values)
+    cur = @head
+    values.each { |value|
+      cur[value] = TrieNode.new unless (cur.has_child? value)
+      cur = cur[value]
+    }
+    cur.exists = true
+  end
+
+  def has_path(values)
+    cur = @head
+    values.each { |value|
+      cur = cur[value]
+      return false if cur.nil?
+    }
+    return cur.exists ? cur : false
+  end
+
+  private
+
+  class String
+    def each
+      each_char { |char| yield char }
+    end
+  end
+
   class TrieNode
     attr_accessor :exists
     def initialize
@@ -23,37 +57,5 @@ class Trie
     def []=(value, node)
       @children[value] = node
     end
-  end
-
-  def initialize(list_of_values = [])
-    @head = TrieNode.new
-    list_of_values.each { |values| add_path(values) }
-  end
-
-  def has_path(values)
-    cur = @head
-    values.each { |value|
-      cur = cur[value]
-      return false if cur.nil?
-    }
-    return true
-  end
-
-  def add_leaf(values)
-    cur = @head
-    values.each { |value|
-      cur[value] = TrieNode.new unless (cur.has_child? value)
-      cur = cur[value]
-    }
-    cur.exists = true
-  end
-
-  def has_leaf(values)
-    cur = @head
-    values.each { |value|
-      cur = cur[value]
-      return false if cur.nil?
-    }
-    return cur.exists ? cur : false
   end
 end
