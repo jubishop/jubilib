62dd350f3fa6c68cf427af908af9886c603ef01a
Justin Bishop <jubishop@gmail.com>
2019-06-16 09:41:06 -0700

    working skip list!  maybe.

--- skiplist.rb
+++ skiplist.rb
@@ -10,8 +10,8 @@ class SkipNode
     return @pointers[level]
   end
 
-  def inspect
-    "Level: #{@level}, Value: #{@value}"
+  def to_s
+    "#{@value}{#{@level}}"
   end
 
   private
@@ -35,22 +35,41 @@ class SkipList
 
     # find top level prev/next for this node
     max_level = [node.level, @head.level].min
-    prev_node, next_node = dive_to_nearest_nodes(node)
+    prev_node, next_node = dive_to_nearest_nodes(node, @head, @head.level)
     node[max_level].next = next_node
     node[max_level].prev = prev_node
+    next_node[max_level].prev = node unless next_node.nil?
+    prev_node[max_level].next = node unless prev_node.nil?
 
     (max_level-1).downto(0) { |search_level|
       prev_node, next_node = find_nearest_nodes(node, prev_node || next_node, search_level)
       node[search_level].next = next_node
       node[search_level].prev = prev_node
+      next_node[search_level].prev = node unless next_node.nil?
+      prev_node[search_level].next = node unless prev_node.nil?
     }
 
     @head = node if (node.level > @head.level or (node.level == @head.level and node[node.level].next == @head))
   end
 
+  def debug_print
+    list = Array.new
+    puts "Head is #{@head}"
+    0.upto(@head.level) { |level|
+      cur = @head
+      cur = cur[level].prev until (cur[level].prev.nil?)
+      until (cur.nil?)
+        list.push(cur.to_s)
+        cur = cur[level].next
+      end
+      puts "Level #{level}: #{list.join(",")}"
+      list.clear
+    }
+  end
+
   private
 
-  def dive_to_nearest_nodes(node, search_node = @head, search_level = @head.level)
+  def dive_to_nearest_nodes(node, search_node, search_level)
     return (search_node.value < node.value) ?
       dive_forward_to_nearest_nodes(node, search_node, search_level) :
       dive_backward_to_nearest_nodes(node, search_node, search_level)
@@ -89,17 +108,10 @@ class SkipList
     end
     return search_node[search_level].prev, search_node
   end
-
-  def debug_print
-    cur = @head
-    cur = cur[0].prev until (cur[0].prev.nil?)
-    until (cur.nil?)
-      puts cur.inspect
-      cur = cur[0].next
-    end
-  end
 end
 
-input = Array.new(10) { rand(100) }
+input = Array.new(20) { rand(100) }
 puts input.join(",")
-SkipList.new(input)
+puts input.sort.join(",")
+skip_list = SkipList.new(input)
+skip_list.debug_print
