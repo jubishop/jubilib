0a5641d2b02e42070ece3fddfcab2ee118a569e4
Justin Bishop <jubishop@gmail.com>
2019-07-24 15:54:50 -0700

    more count stuff

--- count.rb
+++ count.rb
@@ -1,6 +1,30 @@
 class Count
+  include Comparable
+  def <=>(other)
+    if (other.is_a? Numeric)
+      return @count <=> other
+    elsif (other.is_a? Count)
+      return @count <=> other.count
+    else
+      raise "Can't compare #{other.class} with a Count"
+    end
+  end
+
+  attr_accessor :count
   def initialize(count = 0)
     @count = count
+  end
+
+  def add(num)
+    @count += num
+  end
+
+  def subtract(num)
+    @count -= num
+  end
+
+  def set(num)
+    @count = num
   end
 
   def increment
